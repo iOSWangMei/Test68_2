20fc621db64662cf64e14faf01edaf4235fb2ab2
iOSWangMei <mac@laoshidekejian.local>
2016-06-07 13:51:34 +0800

    asdfgh

---
+++ Test2345/Test2345/ppppp.h
@@ -0,0 +1,13 @@
+//
+//  ppppp.h
+//  Test2345
+//
+//  Created by mac on 16/6/7.
+//  Copyright © 2016年 zhiyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ppppp : UIView
+
+@end
---
+++ Test2345/Test2345/ppppp.m
@@ -0,0 +1,21 @@
+//
+//  ppppp.m
+//  Test2345
+//
+//  Created by mac on 16/6/7.
+//  Copyright © 2016年 zhiyou. All rights reserved.
+//
+
+#import "ppppp.h"
+
+@implementation ppppp
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
